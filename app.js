b110b30c33bea4868bc0b37ae3fe34888f1a9541
Shruti Agrawal <shruti98932@gmail.com>
2021-05-27 14:11:03 -0400

    implemented click events

--- app.js
+++ app.js
@@ -1,56 +1,108 @@
 $(document).ready(function(){
   var $app = $('#app');
 
+  // Create new HTML elements
   var $title = $('<h1>Twiddler</h1>');
+  // Create event handler functions
+  var handleTitleClick = function(event) {
+    alert('The title of this page is: ' + event.target.innerText);
+  }
+  // Set event listeners (providing appropriate handlers as input)
+  $title.on('click', handleTitleClick);
+
   $title.appendTo($app);
-
-  // Set a click event listener on the h1 element
-$title.on("click", function(event) {
-  console.log(event);
-  alert('The title of this page is: ' + event.target.innerText);
-});
-
-  var $renderFeed = $('<button #update-feed> Update Feed</button>');
-  $renderFeed.appendTo($app);
 
   var $feed = $('<div id="feed"></div>');
 
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    //console.log(tweet.user)
-    var $tweet = $('<div class="tweet"></div>');
-    var $icon = $('<div class="icon"></div>');
-    if(tweet.user === "douglascalhoun") {
-      $tweet.append('<img id="profile-photo" src="assets/img/douglascalhoun.png" />')
+  var renderFeed = function(user) {
+    if(user === undefined) {
+      var index = streams.home.length - 1;
+      while(index >= 0){
+        var tweet = streams.home[index];
+        var $tweet = $('<div class="tweet"></div>');
+        //var $renderUser = $('<button class="username">' + '@'+tweet.user + '</button>')
+        $tweet.append('<img id="profile-photo" src="assets/img/' + tweet.user + '.png" />')
+        $tweet.append('<div class="username">' + '@'+tweet.user + '</div>')
+        $tweet.append('<div class="message">' + tweet.message + '</div>')
+        $tweet.append('<div class="timestamp">' + jQuery.timeago(tweet.created_at) + '</div>')
+        $tweet.append('<i class="fas fa-comment icon comment"></i>')
+        $tweet.append('<i class="fas fa-retweet icon retweet"></i>')
+        $tweet.append('<i class="far fa-thumbs-up icon like"></i>')
+        $tweet.append('<i class="fas fa-share icon share"></i>')
+        $tweet.appendTo($feed);
+        $feed.appendTo($app);
+        index -= 1;
+      }
+    } else {
+      var username= user.substring(1);
+      console.log( username);
+      console.log( streams.users);
+      var index = streams.users[username];
+      console.log( index);
+      for(var tweet of index) {
+        console.log(tweet);
+        var $tweet = $('<div class="tweet"></div>');
+        $tweet.append('<img id="profile-photo" src="assets/img/' + tweet.user + '.png" />')
+        $tweet.append('<div class="username">' + '@'+tweet.user + '</div>')
+        $tweet.append('<div class="message">' + tweet.message + '</div>')
+        $tweet.append('<div class="timestamp">' + jQuery.timeago(tweet.created_at) + '</div>')
+        $tweet.append('<i class="fas fa-comment icon comment"></i>')
+        $tweet.append('<i class="fas fa-retweet icon retweet"></i>')
+        $tweet.append('<i class="far fa-thumbs-up icon like"></i>')
+        $tweet.append('<i class="fas fa-share icon share"></i>')
+        $tweet.appendTo($feed);
+        $feed.appendTo($app);
+      }
     }
-     if(tweet.user === "mracus") {
-      $tweet.append('<img id="profile-photo" src="assets/img/mracus.png" />')
+
+  }
+
+  // Create new HTML elements
+  var $updateFeed = $('<button id="update-feed"> Update Feed</button>');
+  $updateFeed.appendTo($app);
+
+  renderFeed();
+
+  var handleButtonClick = function(event) {
+    if(event.target.outerText === "Update Feed") {
+      $('.tweet').remove();
+      renderFeed();
     }
-     if(tweet.user === "sharksforcheap") {
-      $tweet.append('<img id="profile-photo" src="assets/img/sharksforcheap.png" />')
+    if(event.target.outerText === "Back") {
+      $('.tweet').remove();
+      $('#update-feed').remove();
+      $updateFeed = $('<button id="update-feed"> Update Feed</button>');
+      $updateFeed.appendTo($app);
+      renderFeed();
     }
-     if(tweet.user === "shawndrost") {
-      $tweet.append('<img id="profile-photo" src="assets/img/shawndrost.png" />')
-    }
-    $tweet.append('<div class="username">' + '@'+tweet.user + '</div>')
-    $tweet.append('<div class="message">' + tweet.message + '</div>')
-    $tweet.append('<div class="timestamp">' + jQuery.timeago(tweet.created_at) + '</div>')
-    $tweet.append('<i class="fas fa-comment icon comment"></i>')
-    $tweet.append('<i class="fas fa-retweet icon retweet"></i>')
-    $tweet.append('<i class="far fa-thumbs-up icon like"></i>')
-    $tweet.append('<i class="fas fa-share icon share"></i>')
-    $tweet.appendTo($feed);
-    $feed.appendTo($app);
-    index -= 1;
+
+    $('.username').on("click", handleUsernameClick);
   }
 //Remove Duplicates
-$renderFeed.on("click", function(event) {
-  $('.tweet').remove();
- index = streams.home.length - 1;
+  $updateFeed.on("click", handleButtonClick);
+
+  var handleUsernameClick = function(event) {
+    $('.tweet').remove();
+    $('#update-feed').remove();
+    $updateFeed = $('<button id="update-feed"> Back</button>');
+    $updateFeed.appendTo($app);
+    renderFeed(event.target.innerText);
+
+    $updateFeed.on("click", handleButtonClick);
+  }
+
+  $('.username').on("click", handleUsernameClick);
+
+});
+
+
+
+
+/*index = streams.home.length - 1;
 while(index >= 0){
   var tweet = streams.home[index];
+  var $renderUser = $('<button class="username">' + '@'+tweet.user + '</button>')
   var $tweet = $('<div class="tweet"></div>');
   if(tweet.user === "douglascalhoun") {
     $tweet.append('<img id="profile-photo" src="assets/img/douglascalhoun.png" />')
@@ -64,7 +116,7 @@ while(index >= 0){
    if(tweet.user === "shawndrost") {
     $tweet.append('<img id="profile-photo" src="assets/img/shawndrost.png" />')
   }
-  $tweet.append('<div class="username">' + '@'+tweet.user + '</div>')
+  $tweet.append($renderUser)
   $tweet.append('<div class="message">' + tweet.message + '</div>')
   $tweet.append('<div class="timestamp">' + jQuery.timeago(tweet.created_at) + '</div>')
   $tweet.append('<i class="fas fa-comment icon comment"></i>')
@@ -73,7 +125,5 @@ while(index >= 0){
   $tweet.append('<i class="fas fa-share icon share"></i>')
   $tweet.prependTo($feed);
     index -= 1;
-  event.preventDefault();
 }
-});
-});
+    */
