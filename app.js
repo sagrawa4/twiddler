ee5de3b435eaaf2500d0fe91352e0adbd45dfcd5
Shruti Agrawal <shruti98932@gmail.com>
2021-05-28 14:05:33 -0400

    window property set to true

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ $(document).ready(function(){
       while(index >= 0){
         var tweet = streams.home[index];
         var $tweet = $('<div class="tweet"></div>');
-        //var $renderUser = $('<button class="username">' + '@'+tweet.user + '</button>')
         $tweet.append('<img class="profile-photo" src="assets/img/' + tweet.user + '.png" />')
         $tweet.append('<button class="username">' + '@'+tweet.user + '</button>')
         $tweet.append('<div class="message">' + tweet.message + '</div>')
@@ -36,12 +35,8 @@ $(document).ready(function(){
       }
     } else {
       var username= user.substring(1);
-      console.log( username);
-      console.log( streams.users);
       var index = streams.users[username];
-      console.log( index);
       for(var tweet of index) {
-        console.log(tweet);
         var $tweet = $('<div class="tweet"></div>');
         $tweet.append('<img class="profile-photo" src="assets/img/' + tweet.user + '.png" />')
         $tweet.append('<button class="username">' + '@'+tweet.user + '</button>')
@@ -94,36 +89,7 @@ $(document).ready(function(){
 
   $('.username').on("click", handleUsernameClick);
 
+  window.isItBeautifulYet = true
+
+
 });
-
-
-
-
-/*index = streams.home.length - 1;
-while(index >= 0){
-  var tweet = streams.home[index];
-  var $renderUser = $('<button class="username">' + '@'+tweet.user + '</button>')
-  var $tweet = $('<div class="tweet"></div>');
-  if(tweet.user === "douglascalhoun") {
-    $tweet.append('<img id="profile-photo" src="assets/img/douglascalhoun.png" />')
-  }
-   if(tweet.user === "mracus") {
-    $tweet.append('<img id="profile-photo" src="assets/img/mracus.png" />')
-  }
-   if(tweet.user === "sharksforcheap") {
-    $tweet.append('<img id="profile-photo" src="assets/img/sharksforcheap.png" />')
-  }
-   if(tweet.user === "shawndrost") {
-    $tweet.append('<img id="profile-photo" src="assets/img/shawndrost.png" />')
-  }
-  $tweet.append($renderUser)
-  $tweet.append('<div class="message">' + tweet.message + '</div>')
-  $tweet.append('<div class="timestamp">' + jQuery.timeago(tweet.created_at) + '</div>')
-  $tweet.append('<i class="fas fa-comment icon comment"></i>')
-  $tweet.append('<i class="fas fa-retweet icon retweet"></i>')
-  $tweet.append('<i class="far fa-thumbs-up icon like"></i>')
-  $tweet.append('<i class="fas fa-share icon share"></i>')
-  $tweet.prependTo($feed);
-    index -= 1;
-}
-    */
